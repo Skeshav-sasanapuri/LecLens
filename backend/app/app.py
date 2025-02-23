d2ca5c5d1d059168e60fcbf3cb550cffc1ed745c
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-02-23 04:40:50 -0500

    update app.py code in backend

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from api.notes import upload_bp
+from api.qa import question_bp
+from api.quiz import upload_bp
+from api.transcript import question_bp
 from api.upload import upload_bp
-from api.question import question_bp
 
 app = Flask(__name__)
 
