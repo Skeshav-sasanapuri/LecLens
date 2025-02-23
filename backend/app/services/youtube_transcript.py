4068e3bae70c77e3c17a73ab43ef9281eaedca02
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-02-22 21:59:01 -0500

    update youtube transcript to return string of transcript

--- backend/app/services/youtube_transcript.py
+++ backend/app/services/youtube_transcript.py
@@ -18,25 +18,30 @@ def extract_video_id(youtube_url: str) -> str:
     raise ValueError("Invalid YouTube URL provided.")
 
 
-def transform_transcript(transcript_list: list) -> dict:
+def transform_transcript(transcript_list: list) -> tuple:
     """
     Transform the transcript list to a dictionary where:
     - Keys are unique sentences.
     - Values are sets of start times when the sentence appears.
     """
     transcript_dict = defaultdict(set)
+    full_transcript = []
 
     for segment in transcript_list:
+        text = segment["text"].replace("\n", " ")
         transcript_dict[segment["text"]].add(segment["start"])
+        full_transcript.append(text)
+    full_transcript_str = " ".join(full_transcript)
+    formatted_transcript_timestamps = {text: sorted(times) for text, times in transcript_dict.items()}
 
-    return {text: sorted(times) for text, times in
-            transcript_dict.items()}  # Convert sets to sorted lists for consistency
+    return formatted_transcript_timestamps, full_transcript_str  # Convert sets to sorted lists for consistency
 
 
-def get_transcript(youtube_url: str) -> dict:
+def get_transcript(youtube_url: str) -> tuple:
     """
     Retrieve and transform the transcript for the given YouTube URL.
     """
     video_id = extract_video_id(youtube_url)
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-    return transform_transcript(transcript_list)
+    transcript_with_timestamps, full_transcript = transform_transcript(transcript_list)
+    return transcript_with_timestamps, full_transcript
