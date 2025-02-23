2fb554ad6204e813fac6374aa217ee511250e0b1
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-02-23 09:08:48 -0500

    fix requirments and upload error

--- backend/app/api/endpoints/upload.py
+++ backend/app/api/endpoints/upload.py
@@ -19,10 +19,10 @@ def upload_video():
     session_id = user.get_sessionId()  # remove this once teja has fixed session id
 
 
-    if data.youtube_url:
-        transcript_time_stamps, transcript_str = youtube_transcript.get_transcript(data.youtube_url)
+    if youtube_url:
+        transcript_time_stamps, transcript_str = youtube_transcript.get_transcript(youtube_url)
     else:
-        transcript_time_stamps, transcript_str = transcript_extraction.get_transcript_from_file(data.video_file)
+        transcript_time_stamps, transcript_str = transcript_extraction.get_transcript_from_file(video_file)
 
     user.set_transcript(transcript_str)
     user.set_transcript_timestamps(transcript_time_stamps)
